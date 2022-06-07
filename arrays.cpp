fec34c8cac1a784527b7b7bd7dc89ee8b079ccd5
ColonelAVP <athervpatil05@gmail.com>
2022-06-07 20:09:31 +0530

    7 june

---
+++ arrays.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int luckyNums[20] = {4, 8, 15, 16, 23, 42};
+
+    luckyNums[0] = 19;
+    luckyNums[6] = 29;
+
+    string name[] = {"Atherv", "Patil", "Guarav", "Pore"};
+
+    cout << luckyNums[0] << endl;
+
+    cout << name[0] << endl;
+}
